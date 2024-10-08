327449e5993141ff11745ce7d7bdeac535935f9f
Rafael Fermino <rlacerdafermino@outlook.com>
2024-10-07 21:45:08 -0300

    Add list_music

--- musicas.py
+++ musicas.py
@@ -1,4 +1,5 @@
 class Musica:
+    musics = [] # List of music
     def __init__(self, nome, categoria, autor):
         self.nome = nome
         self.categoria = categoria
@@ -8,10 +9,21 @@ class Musica:
 #def str para reconhecer mais um metódo próprio Python  
     def __str__(self):
         return f'{self.nome} | {self.categoria} | {self.autor}'
+    
+    def list_music():
+        for music in Musica.musics:
+            print(f'{music.nome} | {music.categoria} | {music.autor}')
 
 
 Musica_1 = Musica('Ela Partiu','Soul','Tim Maia')
 Musica_2 = Musica('So os Loucos Sabem','Hip-Hop','Charlie Brown Jr')
+Musica_3 = Musica('Onda','Soul','Cassiano')
 # print(vars(Musicas_1))
-print(Musica_1)
-print(Musica_2)
+#print(Musica_1)
+#print(Musica_2)]
+
+Musica.musics.append(Musica_1)
+Musica.musics.append(Musica_2)
+Musica.musics.append(Musica_3)
+
+Musica.list_music()
